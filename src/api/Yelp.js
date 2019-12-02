c89857c0cc347ca5ca5e7aeeba03d06f452e0556
bekwdev <bekwdev@gmail.com>
2019-12-02 12:16:45 -0500

    Added initial/default search results and error handling (ErrorMessage  needs customization)

--- src/api/Yelp.js
+++ src/api/Yelp.js
@@ -1,8 +1,26 @@
+// first  install  axios   with terminal  " npm install axios"
+
 import axios from 'axios';
 
 export default axios.create ({
+  //   do not put ( / ) end of  the   url
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
+    // important  !!!!  Authorization    must be   with    capital A
+    // Authorization:  "Bearer  API key"
     Authorization: 'Bearer i1_mHrNigHSvXJm2xAs8dK-bWomBKC5hLy2cPyYdIeCtfMZJHpEbmUE1D2AIXH1ds_HxHbBnTpdQwnc7EnDYnyuH2cBmpA0Mu0rKGLb1LPFZNGCCprZZKIOWrFnhXXYx ',
   },
 });
+
+//    write this function  on  parent screen
+// const searchApi = async () => {
+//     const response = await yelp.get('/search', {
+//         params: {
+//             limit: 50,   //  limit of results
+//             term: State ,     //   value of search input  , in my case State
+//             location: '',    //   name of  city
+//         },
+//     });
+
+//     setResult(response.data.businesses);
+// };
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/Yelp';
@@ -6,27 +6,39 @@ import yelp from '../api/Yelp';
 const SearchScreen = () => {
   const [State, setState] = useState ('');
   const [Result, setResult] = useState ([]);
+  const [ErrorMessage, setErrorMessage] = useState ('');
 
-  const searchApi = async () => {
-    const response = await yelp.get ('/search', {
-      params: {
-        limit: 50,
-        term: State,
-        location: 'san jose',
-      },
-    });
+  const searchApi = async initialState => {
+    try {
+      const response = await yelp.get ('/search', {
+        params: {
+          limit: 50,
+          term: initialState,
+          location: 'san jose',
+        },
+      });
 
-    setResult (response.data.businesses);
+      setResult (response.data.businesses);
+    } catch (err) {
+      setErrorMessage ('Failed , try again later');
+    }
   };
+
+  // searchApi("sushi")             //   do not use this , it causes infinite rendering
+  useEffect (() => {
+    searchApi ('sushi');
+  }, []);
+
   return (
     <View>
       <SearchBar
         state={State}
         // onStateChange={change => setState (change)}   condensed
         onStateChange={setState}
-        onChangeSubmit={searchApi}
+        onChangeSubmit={() => searchApi (term)}
       />
       <Text>We found {Result.length} results</Text>
+      {ErrorMessage ? <Text>{ErrorMessage}</Text> : null}
 
     </View>
   );
