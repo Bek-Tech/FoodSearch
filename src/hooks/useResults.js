ab40c55552537602dd4498db1b9d717c7290c031
bekwdev <bekwdev@gmail.com>
2019-12-02 13:07:10 -0500

    bussines logic from SearchScreen extracted separetly to hooks folder

---
+++ src/hooks/useResults.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import yelp from '../api/Yelp';
+
+export default () => {
+  const [Result, setResult] = useState ([]);
+  const [ErrorMessage, setErrorMessage] = useState ('');
+
+  const searchApi = async initialState => {
+    try {
+      const response = await yelp.get ('/search', {
+        params: {
+          limit: 50,
+          term: initialState,
+          location: 'san jose',
+        },
+      });
+
+      setResult (response.data.businesses);
+    } catch (err) {
+      setErrorMessage ('Failed , try again later');
+    }
+  };
+
+  // searchApi("sushi")             //   do not use this , it causes infinite rendering
+  useEffect (() => {
+    searchApi ('sushi');
+  }, []);
+  return [searchApi, Result, ErrorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/Yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [State, setState] = useState ('');
-  const [Result, setResult] = useState ([]);
-  const [ErrorMessage, setErrorMessage] = useState ('');
-
-  const searchApi = async initialState => {
-    try {
-      const response = await yelp.get ('/search', {
-        params: {
-          limit: 50,
-          term: initialState,
-          location: 'san jose',
-        },
-      });
-
-      setResult (response.data.businesses);
-    } catch (err) {
-      setErrorMessage ('Failed , try again later');
-    }
-  };
-
-  // searchApi("sushi")             //   do not use this , it causes infinite rendering
-  useEffect (() => {
-    searchApi ('sushi');
-  }, []);
+  const [searchApi, Result, ErrorMessage] = useResults ();
 
   return (
     <View>
